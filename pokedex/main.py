504ef6a3e6377e9aa5609cb1ff0893737570af5f
Miroslav Biňas <miroslav.binas@tuke.sk>
2023-07-04 13:00:41 +0200

    refactoring: creating package for api - WIP

--- pokedex/main.py
+++ pokedex/main.py
@@ -16,40 +16,7 @@ admin = Admin(app, engine)
 admin.add_view(PokemonAdmin)
 
 
-@app.get('/api/pokemons')
-def get_list_of_pokemons(classification: str = None, offset: int = 0, page_size: int = 10):
-    pokemons_total = 801
 
-    with Session(engine) as session:
-        statement = select(Pokemon).offset(offset * page_size).limit(page_size)
-
-        if classification is not None:
-            statement = statement.where(Pokemon.classification == classification)
-
-        pokemons = session.exec(statement).all()
-
-    return {
-        'first': f'/api/pokemons?page_size={page_size}',
-        'previous': f'/api/pokemons?page_size={page_size}&offset={offset - 1}',
-        'next': f'/api/pokemons?page_size={page_size}&offset={offset + 1}',
-        'last': f'/api/pokemons?page_size={page_size}&offset={math.ceil(pokemons_total / page_size)}',
-        'results': pokemons
-    }
-
-
-@app.get('/api/pokemons/{pokedex_number}')
-def get_list_of_pokemons(pokedex_number: int):
-    with Session(engine) as session:
-        statement = select(Pokemon).where(Pokemon.pokedex_number == pokedex_number)
-        pokemon = session.exec(statement).one_or_none()
-
-        if pokemon is None:
-            raise HTTPException(
-                status_code=404,
-                detail=f'Pokémon with pokédex number {pokedex_number} not found.',
-            )
-
-    return pokemon
 
 
 if __name__ == '__main__':
