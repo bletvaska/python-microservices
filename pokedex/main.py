2dbc8add8131f1e7fbdba9f3fb9e2b4d3262d5c5
Miroslav Biňas <miroslav.binas@tuke.sk>
2023-07-06 13:12:31 +0200

    added static endpoint

--- pokedex/main.py
+++ pokedex/main.py
@@ -4,6 +4,7 @@ import uvicorn as uvicorn
 from fastapi import FastAPI, Request
 from fastapi_pagination import add_pagination
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from sqladmin import Admin
 from sqlmodel import create_engine
 from starlette.templating import Jinja2Templates
@@ -15,6 +16,7 @@ from pokedex.models.pokemon import PokemonAdmin
 app = FastAPI()
 app.include_router(pokemons_router)
 add_pagination(app)
+app.mount('/static', StaticFiles(directory='pokedex/static'), name='static')
 templates = Jinja2Templates(directory='pokedex/templates/')
 
 # db engine
