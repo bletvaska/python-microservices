5b578c9fd37b8d8e1ababc3965304d93c2c4f43d
Miroslav Biňas <miroslav.binas@tuke.sk>
2023-07-07 10:27:03 +0200

    added http status code 500 if not healthy

--- pokedex/api/healthcheck.py
+++ pokedex/api/healthcheck.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from fastapi import APIRouter, Depends
+from fastapi.responses import JSONResponse
 from sqlmodel import Session
 
 from pokedex.dependencies import get_session
@@ -22,8 +23,13 @@ def healthcheck(session: Session = Depends(get_session)):
     db_state = _is_db_healthy(session)
     static_folder_state = _static_folder_exists()
 
-    return {
-        'status': 'ok',
-        'dbState': db_state,
-        'staticFolderExist': static_folder_state
-    }
+    is_healthy = db_state and static_folder_state
+
+    return JSONResponse(
+        status_code=200 if is_healthy else 500,
+        content={
+            'isHealthy': is_healthy,
+            'dbState': db_state,
+            'staticFolderExist': static_folder_state
+        }
+    )
