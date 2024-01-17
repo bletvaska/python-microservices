083625a16223ef7ed2a31e4205f2ab3cd05513ba
mirek <mirek@cnl.sk>
2024-01-17 13:33:11 +0100

    dependency injection

--- weather/api/measurements.py
+++ weather/api/measurements.py
@@ -1,29 +1,22 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from sqlmodel import create_engine, Session, select
 
-from weather.dependencies import get_settings
+from weather.dependencies import get_settings, get_session
 from weather.models.measurement import Measurement
 
 router = APIRouter()
 
 
 @router.get("/api/measurements")
-def get_all_measurements():
-    engine = create_engine(get_settings().db_uri)
+def get_all_measurements(session: Session = Depends(get_session)):
+    statement = select(Measurement)
+    # if city is not None:
+    #     statement = statement.where(Measurement.city == city)
 
-    with Session(engine) as session:
-        statement = select(Measurement)
-        # if city is not None:
-        #     statement = statement.where(Measurement.city == city)
-
-        return session.exec(statement).all()
+    return session.exec(statement).all()
 
 
 @router.get('/api/measurements/last')
-def get_last_measurement():
-    engine = create_engine(get_settings().db_uri)
-
-    with Session(engine) as session:
-        statement = select(Measurement).order_by(Measurement.id.desc())
-
-        return session.exec(statement).first()
+def get_last_measurement(session: Session = Depends(get_session)):
+    statement = select(Measurement).order_by(Measurement.id.desc())
+    return session.exec(statement).first()
--- weather/dependencies.py
+++ weather/dependencies.py
@@ -1,6 +1,7 @@
 from functools import lru_cache
 
 from loguru import logger
+from sqlmodel import create_engine, Session
 
 from weather.models.settings import Settings
 
@@ -10,3 +11,9 @@ def get_settings() -> Settings:
     settings = Settings()
     logger.info(f'Loading settings for {settings.environment} environment')
     return settings
+
+
+def get_session():
+    engine = create_engine(get_settings().db_uri)
+    with Session(engine) as session:
+        yield session
