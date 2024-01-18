fb67c594191a45c3cf900ed2343efe13772b3705
mirek <mirek@cnl.sk>
2024-01-18 15:15:11 +0100

    sablona funguje

--- weather/views/homepage.py
+++ weather/views/homepage.py
@@ -1,8 +1,16 @@
-from fastapi import APIRouter
+from pathlib import Path
+
+from fastapi import APIRouter, Request
+from fastapi.templating import Jinja2Templates
 
 router = APIRouter()
 
 
 @router.get('/')
-def homepage():
-    return 'hello world'
+def homepage(request: Request):
+    context = {
+        'request': request
+    }
+    templates = Jinja2Templates(directory=Path(__file__).parent.parent / 'templates')
+
+    return templates.TemplateResponse('hello.html', context)
