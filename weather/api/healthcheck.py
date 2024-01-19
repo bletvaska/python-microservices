cf75fcb95fc56e3f99fd287ad9b73314814491c9
mirek <mirek@cnl.sk>
2024-01-19 11:46:35 +0100

    refaktoring

--- weather/api/healthcheck.py
+++ weather/api/healthcheck.py
@@ -1,10 +1,37 @@
+import http
+
 from fastapi import APIRouter
+from starlette.responses import JSONResponse
+
+from weather.dependencies import get_session
 
 router = APIRouter()
 
 
+def is_disk_healthy():
+    return get_session()
+
+
+def is_db_healthy():
+    return False
+
+
+@router.head('/health')
 @router.get("/health")
 def healthcheck():
-    return {
-        'status': 'ok'
-    }
+    disk = is_disk_healthy()
+    db = is_db_healthy()
+
+    if disk and db:
+        status = http.HTTPStatus.OK
+    else:
+        status = http.HTTPStatus.INTERNAL_SERVER_ERROR
+
+    return JSONResponse(
+        status_code=status,
+        content={
+            'status': disk and db,
+            'disk': disk,
+            'database': db
+        }
+    )
