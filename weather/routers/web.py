1df2a3fff71a7a9e152c4b4f6d12515e604574bd
mirek <mirek@cnl.sk>
2024-12-11 15:16:17 +0100

    nacitanie sablony pomocou jinja2

--- weather/routers/web.py
+++ weather/routers/web.py
@@ -1,7 +1,13 @@
+from pathlib import Path
+
 from fastapi import APIRouter
+from starlette.responses import HTMLResponse
+from starlette.templating import Jinja2Templates
 
 router = APIRouter()
 
-@router.get('/hello')
+@router.get('/hello', response_class=HTMLResponse)
 def hello():
-    return 'hello world'
+    templates = Jinja2Templates(directory=Path(__file__).parent.parent / 'templates')
+    template = templates.get_template('hello.html')
+    return template.render()
