f04d15633fbd83c17326d1293dc6818183931b2f
mirek <mirek@cnl.sk>
2024-12-11 10:27:36 +0100

    premenne prostredia v settings

--- weather/models/settings.py
+++ weather/models/settings.py
@@ -6,10 +6,10 @@ class Settings(BaseSettings):
     interval: int = 60
     db_uri: str = 'sqlite:///weather.sqlite'
     cities: list[str] = []
-    # log_level: Literal['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'] = 'INFO'
 
     model_config = SettingsConfigDict(
         env_file_encoding='utf-8',
-        env_file='.env',
+        # env_file='.env',
         env_prefix='WEATHER_',
+        extra='allow',
     )
